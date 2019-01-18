5015877ffb9f9421ab1be9fa54a9faff15912eb1
ioana <ioana.valentir@gmail.com>
2019-01-18 18:15:25 +0100

    added class for arraylist object

---
+++ Owners.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class Owners{
+  public static ArrayList<Owners> owners = new ArrayList<Owners>();
+  String owner_id;
+  String person_id;
+  String car_id;
+
+
+}
