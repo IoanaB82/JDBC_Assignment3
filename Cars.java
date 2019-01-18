818594d0e49c8389c8231da38b90e7ead12e05c6
ioana <ioana.valentir@gmail.com>
2019-01-18 18:14:25 +0100

    added file for arrayList class to help the main code

---
+++ Cars.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class Cars{
+  public static ArrayList<Cars> cars = new ArrayList<Cars>();
+  String car_id;
+  String brand;
+  String color;
+
+
+}
