14c2b71382d300bcf6b99c8e2df9490812f94de1
ioana <ioana.valentir@gmail.com>
2019-01-18 18:15:41 +0100

    added class for arraylist object

---
+++ People.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class People{
+  public static ArrayList<People> people = new ArrayList<People>();
+  String people_id;
+  String firstname;
+  String lastname;
+
+
+}
